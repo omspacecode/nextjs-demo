76216252968639fd570a41050311ad7ff3c2eaf5
OmPrakash RajPurohit <om1719@gmail.com>
2023-08-10 15:36:04 +0530

    Update [[...page]].tsx

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -43,7 +43,7 @@ export async function getStaticProps({
   
   const dataModel = await builder.getAll('artworks', {
         options: {
-          vercelPreview: true,
+          vercelPreview: process.env.VERCEL_ENV === 'preview',
         },
       })
   
